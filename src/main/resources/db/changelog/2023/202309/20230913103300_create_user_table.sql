106ec7b3ef9c9a5152403bec692d2535f546e93e
tosin-cbsystems <121865559+tosin-cbsystems@users.noreply.github.com>
2023-09-13 11:13:19 +0100

    set up liquibase and database and add user table

---
+++ src/main/resources/db/changelog/2023/202309/20230913103300_create_user_table.sql
@@ -0,0 +1,15 @@
+--liquibase formatted sql
+--changeset iyiola.oluwatosin:20230913103300_create_user_table
+
+CREATE TABLE IF NOT EXISTS users(
+    id              UUID PRIMARY KEY,
+    username        VARCHAR(30) NULL UNIQUE,
+    email           VARCHAR(50) NOT NULL UNIQUE,
+    role            VARCHAR(20) NOT NULL,
+    password        TEXT NOT NULL,
+    created_on      TIMESTAMP(3) without time zone DEFAULT (now() at time zone 'utc') NOT NULL,
+    updated_at      TIMESTAMP(3) without time zone DEFAULT (now() at time zone 'utc') NOT NULL
+);
+
+CREATE INDEX IF NOT EXISTS users_username_index ON users (username);
+CREATE INDEX IF NOT EXISTS users_email_index ON users (email);
